53499314a2313d10b946864aa44a13c24717fb72
KC <kimchauduong@gmail.com>
2021-09-15 19:54:03 +0200

    Overload constructor in depending on argument type

--- src/main/java/com/example/ikm/NutrionalValue.java
+++ src/main/java/com/example/ikm/NutrionalValue.java
@@ -7,15 +7,38 @@ package com.example.ikm;
  * @version 1.0
  */
 public class NutrionalValue {
-    String calories;
-    String fat;
-    String carbohydrates;
-    String protein;
+    Double calories;
+    Double fat;
+    Double carbohydrates;
+    Double protein;
 
     public NutrionalValue(String calories, String fat, String carbohydrates, String protein) {
+        this.calories = Double.parseDouble(calories);
+        this.fat = Double.parseDouble(fat);
+        this.carbohydrates = Double.parseDouble(carbohydrates);
+        this.protein = Double.parseDouble(protein);
+    }
+
+    public NutrionalValue(Double calories, Double fat, Double carbohydrates, Double protein) {
         this.calories = calories;
         this.fat = fat;
         this.carbohydrates = carbohydrates;
         this.protein = protein;
+    }
+
+    public Double getCalories() {
+        return calories;
+    }
+
+    public Double getFat() {
+        return fat;
+    }
+
+    public Double getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public Double getProtein() {
+        return protein;
     }
 }
